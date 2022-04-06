0710a12aea5e7c53399390ef663c34791e9c5046
Doucara Fodié <fodiedoucara8@gmail.com>
2022-04-06 15:45:30 +0200

    ajout du middleware multer sur les bonnes routes

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -7,7 +7,7 @@ const sauceSchema = mongoose.Schema({
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
-  heat: { type: Number },
+  heat: { type: Number, required: true },
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
   usersLiked: { type: [String] },
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,11 +5,11 @@ const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-config');
 
-router.get('/', auth, multer, sauceCtrl.getAllSauces);
-router.get('/:id', auth, multer, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauces);
 router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.put('/:id', auth, multer, sauceCtrl.updateOneSauce);
-router.delete('/:id', auth, multer, sauceCtrl.deleteOneSauce);
+router.delete('/:id', auth, sauceCtrl.deleteOneSauce);
 
 module.exports = router;
